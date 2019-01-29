c1711eff2772ee44ee428b31457c5d2766b02fce
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-01-29 22:46:40 +0000

    Created 3 methods to GET 3 different files...akacallback hell, next we will show a simply solution

--- async.js
+++ async.js
@@ -1,14 +1,81 @@
 window.onload = function(){
-    
-    var http = new XMLHttpRequest();
-    
-    http.onreadystatechange = function() {
-        if(http.readyState == 4 && http.status == 200){
-            console.log(JSON.parse(http.response));
-        }
-    };
-    
-    http.open("GET", "data/tweets.json", true);
-    http.send();
-    console.log("test");
+  $.ajax({
+        type: "GET", 
+        url: "data/tweets.json",
+        success: function(data){
+            console.log(data);
+     $.ajax({
+        type: "GET", 
+        url: "data/friends.json",
+        success: function(data){
+            console.log(data);
+           
+         $.ajax({
+        type: "GET", 
+        url: "data/videos.json",
+        success: function(data){
+            console.log(data);
+            
+            },
+            
+        error: function(jqXHR, textStatus, error){
+        console.log(error);
+      }
+    });   
+ },
+            
+        error: function(jqXHR, textStatus, error){
+        console.log(error);
+      }
+    });
+  },
+            
+        error: function(jqXHR, textStatus, error){
+        console.log(error);
+      }
+   });
 };
+
+ 
+//     function cb(data) {
+//         console.log(data);
+//     }
+//     $.get("data/tweets.json", cb);
+//     console.log("test");
+// };
+
+
+
+
+//     function callback(val){
+//         console.log(val);
+//     }
+    
+//     var fruits = ["banana", "apple", "pineapple"];
+//     fruits.forEach(function(){
+//         console.log.log("done");
+//     });
+    
+    
+
+
+
+//     var http = new XMLHttpRequest();
+    
+//     http.onreadystatechange = function() {
+//         if(http.readyState == 4 && http.status == 200){
+//           // console.log(JSON.parse(http.response));
+//         }
+//     };
+    
+//     http.open("GET", "data/tweets.json", true);
+//     http.send();
+//   // console.log("test");
+   
+//   //jquery method
+//   $.get("data/tweets.json", function(data){
+//       console.log(data);
+//   });
+//   console.log(test);
+   
+// }; 
