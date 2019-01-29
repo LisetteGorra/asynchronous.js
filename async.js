3d3d06bf42b3b78eb7ff0f18d92ec171b265a6ce
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-01-29 22:58:11 +0000

    Created a solution to call back hell, by reshaping functions

--- async.js
+++ async.js
@@ -1,13 +1,29 @@
 window.onload = function(){
+    
+    function handleError(jqXHR, textStatus, error){
+        console.log(error);
+    }
+  
   $.ajax({
         type: "GET", 
         url: "data/tweets.json",
-        success: function(data){
+        success: cbTweets, 
+        error: handleError
+   });
+   
+   function cbTweets(data){
             console.log(data);
+            
      $.ajax({
         type: "GET", 
         url: "data/friends.json",
-        success: function(data){
+        success: cbFriends,
+        error: handleError
+    });
+    
+  }
+  
+  function cbFriends(data){
             console.log(data);
            
          $.ajax({
@@ -16,24 +32,12 @@ window.onload = function(){
         success: function(data){
             console.log(data);
             
-            },
+     },
             
-        error: function(jqXHR, textStatus, error){
-        console.log(error);
-      }
+        error: handleError
+      
     });   
- },
-            
-        error: function(jqXHR, textStatus, error){
-        console.log(error);
-      }
-    });
-  },
-            
-        error: function(jqXHR, textStatus, error){
-        console.log(error);
-      }
-   });
+   }
 };
 
  
