37ef73b5b53538f63e3f3ae4cfaa40a8591f67d7
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-01-30 16:10:39 +0000

    Used generators with asynchronous code, genwrap, handle function, yielded.value.then

--- async.js
+++ async.js
@@ -1,15 +1,31 @@
 window.onload = function(){
-    //generators
-    function* gen(){
-        var x = yield 10;
-        console.log(x);
+ 
+ genWrap(function*(){
+     
+     var tweets = yield $.get("data/tweets.json");
+     console.log(tweets);
+      var friends = yield $.get("data/friends.json");
+     console.log(friends);
+      var videos = yield $.get("data/videos.json");
+     console.log(videos);
+     
+ });
+ 
+    function genWrap(generator){
+        
+        var gen = generator();
+        
+        function handle(yielded){
+            if(!yielded.done){
+                yielded.value.then(function(data){
+                    return handle(gen.next(data));
+                });
+            }
+        }
+        
+        return handle(gen.next());
     }
-    
-    var myGen = gen();
-    console.log(myGen.next());
-    console.log(myGen.next(10));
-    
-    
+};
     
     
     
