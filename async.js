488ba7dfe31f5cc5b581dbe6bda5780321a9a9df
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-01-30 00:00:59 +0000

    Made a Promise and used promise interface, .catch, .then. var xhttp. onerror, with 3 cb functions in simple code

--- async.js
+++ async.js
@@ -1,81 +1,32 @@
 window.onload = function(){
-    
- $.ajax({
-        type: "GET", 
-        url: "data/tweets.json",
-        success: function(data){
-            console.log(data);
-     $.ajax({
-        type: "GET", 
-        url: "data/friends.json",
-        success: function(data){
-            console.log(data);
 
-         $.ajax({
-        type: "GET", 
-        url: "data/videos.json",
-        success: function(data){
-            console.log(data);
-
-            },
-
-        error: function(jqXHR, textStatus, error){
-        console.log(error);
-      }
-    });   
- },
-
-        error: function(jqXHR, textStatus, error){
-        console.log(error);
-      }
-    });
-  },
-
-        error: function(jqXHR, textStatus, error){
-        console.log(error);
-      }
-   });
-};
- 
-//     function cb(data) {
-//         console.log(data);
-//     }
-//     $.get("data/tweets.json", cb);
-//     console.log("test");
-// };
-
-
-
-
-//     function callback(val){
-//         console.log(val);
-//     }
-    
-//     var fruits = ["banana", "apple", "pineapple"];
-//     fruits.forEach(function(){
-//         console.log.log("done");
-//     });
-    
-    
-
-
-
-//     var http = new XMLHttpRequest();
-    
-//     http.onreadystatechange = function() {
-//         if(http.readyState == 4 && http.status == 200){
-//           // console.log(JSON.parse(http.response));
-//         }
-//     };
-    
-//     http.open("GET", "data/tweets.json", true);
-//     http.send();
-//   // console.log("test");
-   
-//   //jquery method
-//   $.get("data/tweets.json", function(data){
-//       console.log(data);
-//   });
-//   console.log(test);
-   
-// }; 
+ function get(url){
+     return new Promise(function(resolve,reject){
+         var xhttp = new XMLHttpRequest();
+         xhttp.open("GET", url, true);
+         xhttp.onload = function(){
+             if(xhttp.status == 200){
+                 resolve(JSON.parse(xhttp.response));
+             } else{
+                 reject(xhttp.statusText);
+             }
+            };
+            xhttp.onerror = function(){
+                reject(xhttp.statusText);
+            };
+            xhttp.send();
+            });
+         }
+         var promise = get("data/tweets.json");
+         promise.then(function(tweets){
+             console.log(tweets);
+             return get("data/friends.json");
+         }).then(function(friends){
+             console.log(friends);
+             return get("data/videos.json");
+         }).then(function(videos){
+             console.log(videos);
+         }).catch(function(error){
+             console.log(error);
+     });
+ };
